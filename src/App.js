f3d7852f5a0f2944d27f0e0702ca149165379035
Emmanuel <emmanu-0000@hotmail.com>
2019-08-04 14:47:22 -0500

    Módulos de Node arreglados y bootstrap eliminado

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import React from "react";
-import Formulario from "./formulario";
-import Cabecera from "./cabecera";
-import Resultado from "./resultado";
+import Agregar from "./components/agregar";
+import Principal from "./components/principal";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-class App extends React.Component {
-  state = {
-    form: {
-      nombre: "",
-      apellido: "",
-      correo: "",
-      trabajo: ""
-    }
-  };
-
-  //Levantando el estado de Formulario a a Resultados
-  handleChange = e => {
-    //Guardar estado
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <Cabecera nombre={this.state.form.nombre} />
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-4">
-              <Resultado
-                nombre={this.state.form.nombre}
-                trabajo={this.state.form.trabajo}
-              />
-            </div>
-            <div className="col-4">
-              <Formulario
-                onChange={this.handleChange}
-                formvalues={this.state.form}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/principal" component={Principal} />
+        <Route exact path="/principal/agregar" component={Agregar} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
