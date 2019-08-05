8358405353c1521d98ba8924c62f7d70340271d7
Emmanuel <emmanu-0000@hotmail.com>
2019-08-05 15:10:44 -0500

    Agregada versión estable de Firebase

---
+++ src/components/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyB1biwZnrlT-hE4ugk-5ino6y8O9jTDs08",
+  authDomain: "drone-mochila-37147.firebaseapp.com",
+  databaseURL: "https://drone-mochila-37147.firebaseio.com",
+  projectId: "drone-mochila-37147",
+  storageBucket: "",
+  messagingSenderId: "1038241953479",
+  appId: "1:1038241953479:web:733fd25e9308d908"
+};
+firebase.initializeApp(config);
+export default firebase;
