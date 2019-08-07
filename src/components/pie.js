9c1dc247d688e68e1a194f1e58cb7d956f629326
Emmanuel <emmanu-0000@hotmail.com>
2019-08-07 12:24:31 -0500

    Pie de página

---
+++ src/components/pie.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Pie extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="footer">
+          <p>
+            Desarrollado por <strong>Manu</strong> 🐯⚡
+          </p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Pie;
